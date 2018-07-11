2ab081c4b4b2c19a391463bc71315a88a297487b
valromo <valroldanmotos@gmail.com>
2018-07-11 20:30:06 +0200

    sin favoritos

--- js/main.js
+++ js/main.js
@@ -8,22 +8,30 @@ var listContainer= document.querySelector(".lista");
 
 function showFilms(){
   var textImput = input.value;
-  fetch('http://api.tvmaze.com/search/shows?q='+textImput)
+  fetch('http://api.tvmaze.com/search/shows?q='+ textImput)
     .then(function(response){
       return response.json();
     })
     .then(function(json){
-      console.log(json);
       for (var i = 0; i < json.length; i++) {
-        var arrayFilms = json[i];
-        var arrayFilmsShow = arrayFilms.show.name;
-        var arrayFilmsImagen = arrayFilms.show.image.medium;
-        var arrayFilmsUrl = arrayFilms.show.url;
-        console.log (arrayFilmsImagen);
-
+        // var arrayFilms = json[i];
+        // var arrayFilmsShow = arrayFilms.show.name;
+        // var arrayFilmsImagen = arrayFilms.show.image;
+        var listContainer= document.querySelector(".lista");
+        var newLi = document.createElement('li');
+        var h2 = document.createElement('h2');
+        var titleText = document.createTextNode (json[i].show.name);
+        var image = document.createElement('img');
+        if (json[i].show.image === null ) {
+          image.src = "https://via.placeholder.com/210x295/cccccc/666666/?text=TV";
+        } else {
+          image.src = json[i].show.image.medium;
+        }
+        h2.appendChild(titleText);
+        newLi.appendChild(h2);
+        newLi.appendChild(image);
+        listContainer.appendChild(newLi);
       }
-
-
     });
 }
 button.addEventListener('click',showFilms);
