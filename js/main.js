86abff090208a4d7dcf367a8b680dea5fd69f240
valromo <valroldanmotos@gmail.com>
2018-07-13 16:20:46 +0200

    revision guay

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ var button = document.querySelector(".boton");
 var input = document.querySelector(".input");
 var textImput = input.value;
 var listContainer= document.querySelector(".lista");
+var button2 = document.querySelector(".boton2");
+var input2 = document.querySelector(".input2");
+var valorNombre=input2.value;
+var parrafo= document.querySelector(".parrafo");
 
 function showSeries(){
   var textImput = input.value;
@@ -40,6 +44,11 @@ function showSeries(){
     });
 }
 button.addEventListener('click',showSeries);
+button2.addEventListener('click',showName);
 function showFavorite(event){
   event.currentTarget.classList.toggle('favorit-serie');
+}
+function showName(evento){
+  alert( "Bienvenida " + input2.value);
+  parrafo.innerHTML= "Bienvenida " + input2.value;
 }
