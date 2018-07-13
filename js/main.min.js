9cb415979ddc85218be5ef79b5a96884a1ad6d45
valromo <valroldanmotos@gmail.com>
2018-07-13 16:27:00 +0200

    personas

--- js/main.min.js
+++ js/main.min.js
@@ -11,7 +11,7 @@ var parrafo= document.querySelector(".parrafo");
 
 function showSeries(){
   var textImput = input.value;
-  fetch('http://api.tvmaze.com/search/shows?q='+ textImput)
+  fetch('http://api.tvmaze.com/search/people?q='+ textImput)
     .then(function(response){
       return response.json();
     })
@@ -19,20 +19,21 @@ function showSeries(){
       var listContainer= document.querySelector(".lista");
       listContainer.innerHTML='';
       for (var i = 0; i < json.length; i++) {
+        console.log(json[i]);
         var newLi = document.createElement('li');
         newLi.classList.add('list-serie');
         newLi.addEventListener('click',showFavorite);
 
         var h2 = document.createElement('h2');
         h2.classList.add('title-serie');
-        var titleText = document.createTextNode (json[i].show.name);
+        var titleText = document.createTextNode (json[i].person.name);
 
         var image = document.createElement('img');
         image.classList.add('image-serie');
-        if (json[i].show.image === null ) {
+        if (json[i].person.image === null ) {
           image.src = 'https://via.placeholder.com/210x295/cccccc/666666/?text=TV';
         } else {
-          image.src = json[i].show.image.medium;
+          image.src = json[i].person.image.medium;
         }
         h2.appendChild(titleText);
         newLi.appendChild(h2);
